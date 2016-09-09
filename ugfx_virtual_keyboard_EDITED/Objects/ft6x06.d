d0316b6e9e15bbfc605f70a8b77f9b717a737574
Jonathan Lukas <jlgoldnauvogt@gmail.com>
2016-09-09 16:10:22 -0400

    removed unneeded files

--- ugfx_virtual_keyboard_EDITED/Objects/ft6x06.d
+++
@@ -1,4 +0,0 @@
-.\objects\ft6x06.o: STM32CubeF4\Drivers\BSP\Components\ft6x06\ft6x06.c
-.\objects\ft6x06.o: STM32CubeF4\Drivers\BSP\Components\ft6x06\ft6x06.h
-.\objects\ft6x06.o: STM32CubeF4\Drivers\BSP\Components\ft6x06\../Common/ts.h
-.\objects\ft6x06.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
--- ugfx_virtual_keyboard_EDITED/Objects/otm8009a.d
+++
@@ -1,3 +0,0 @@
-.\objects\otm8009a.o: STM32CubeF4\Drivers\BSP\Components\otm8009a\otm8009a.c
-.\objects\otm8009a.o: STM32CubeF4\Drivers\BSP\Components\otm8009a\otm8009a.h
-.\objects\otm8009a.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
