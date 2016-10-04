180f8339353398eb49b72eb215067b483d8c0b2b
Ali Ba Wazir <alwaziry20@gmail.com>
2016-10-04 02:01:55 -0400

    Added a function to cscs_app to decode data for wheel and crank revolution, travel distance, speed and travel cadence. These values are printed whenever data is received from sensor

--- NRF51822/examples/ble_central/experimental/ble_app_cscs_c/pca10028/s130/arm5_no_packs/cscs_app.c
+++ NRF51822/examples/ble_central/experimental/ble_app_cscs_c/pca10028/s130/arm5_no_packs/cscs_app.c
@@ -24,22 +24,165 @@
 #include "nrf_log_ctrl.h"
 #include "peer_manager.h"
 
-
 /**********************************************************************************************
 * MACRO DEFINITIONS
 ***********************************************************************************************/
 
+#define DFAULT_WHEEL_DIAMETER 0.5
+#define PI                    3.14159265
 
+/**********************************************************************************************
+* TYPE DEFINITIONS
+***********************************************************************************************/
+typedef struct{
+		uint32_t value;
+		bool     is_read;
+}uint32_data_field_t;
+
+typedef struct{
+		double   value;
+		bool     is_read;
+}double_data_field_t;
+
+typedef struct{
+		uint32_data_field_t oldWheelRevolution;
+		uint32_data_field_t oldCrankRevolution;
+		double_data_field_t travelDistance;
+	  double_data_field_t totalTravelDistance;
+	  double_data_field_t speed; /*km/h*/
+	  double_data_field_t travel_cadence;
+		double_data_field_t oldWheelEventTime; 
+		double_data_field_t oldCrankEventTime;
+} cscs_instantanious_data_t;
 
 /**********************************************************************************************
 * STATIC VARIABLES
 ***********************************************************************************************/
-static ble_cscs_c_t            m_ble_cscs_c;      /**< Structure used to identify the Cycling Speed and Cadence client module. */
-
-
+static ble_cscs_c_t                 m_ble_cscs_c;      /**< Structure used to identify the Cycling Speed and Cadence client module. */
+static cscs_instantanious_data_t    cscs_instantanious_data;
+static double                       wheelCircumference = 2*PI*DFAULT_WHEEL_DIAMETER;               /*The circumfrance is calculated by wheel diameter specified by user*/
+/* TODO: create a function to update wheelCircumference by user data*/
 /**********************************************************************************************
 * STATIC FUCNCTIONS
 ***********************************************************************************************/
+
+static double process_wheel_data(uint32_t new_cumulative_wheel_revs, uint16_t new_wheel_event_time)
+{
+    /* This function called only when wheel Revolution Data Present
+     * Last Wheel Event Time unit has resolution of 1/1024 seconds
+     */
+    
+    double wheel_revolution_diff  = 0.0;
+    double wheel_event_time_diff  = 0.0;
+    double travel_speed           = 0.0;
+    
+    if (cscs_instantanious_data.oldWheelRevolution.value != 0) {
+        wheel_revolution_diff = new_cumulative_wheel_revs - cscs_instantanious_data.oldWheelRevolution.value;
+			/*TODO: create static function for updating*/
+			  cscs_instantanious_data.travelDistance.value = cscs_instantanious_data.travelDistance.value + ((wheel_revolution_diff * wheelCircumference) / 1000.0);
+        cscs_instantanious_data.travelDistance.is_read = false;
+			  cscs_instantanious_data.totalTravelDistance.value = ((new_cumulative_wheel_revs * wheelCircumference) / 1000.0);
+			  cscs_instantanious_data.totalTravelDistance.is_read= false;
+    }
+    if (cscs_instantanious_data.oldWheelEventTime.value != 0) {
+        wheel_event_time_diff = ((new_wheel_event_time - cscs_instantanious_data.oldWheelEventTime.value));
+    }
+    if (wheel_event_time_diff > 0) {
+        wheel_event_time_diff = wheel_event_time_diff / 1024.0;
+        //convert speed from m/s to km/h by multiplying 3.6
+        travel_speed = (((wheel_revolution_diff * wheelCircumference)/wheel_event_time_diff)*3.6);
+
+				cscs_instantanious_data.speed.value = travel_speed;
+			  cscs_instantanious_data.speed.is_read = false;
+    }
+    
+    cscs_instantanious_data.oldWheelRevolution.value = new_cumulative_wheel_revs;
+		cscs_instantanious_data.oldWheelRevolution.is_read = false;
+    cscs_instantanious_data.oldWheelEventTime.value = new_wheel_event_time;
+		cscs_instantanious_data.oldWheelEventTime.is_read = false;
+    
+		return wheel_revolution_diff;
+}
+
+static double process_crank_data(uint16_t new_cumulative_crank_revs, uint16_t new_crank_event_time)
+{
+    /* This function called only when Crank Revolution data present
+     * Last Crank Event Time unit has a resolution of 1/1024 seconds
+     */
+    
+    double      crank_revolution_diff = 0.0;
+    double      crank_event_time_diff = 0.0;
+    double      travel_cadence        = 0.0;
+
+    if (cscs_instantanious_data.oldCrankEventTime.value != 0) {
+        crank_event_time_diff = new_crank_event_time - cscs_instantanious_data.oldCrankEventTime.value;
+    }
+    if (cscs_instantanious_data.oldCrankRevolution.value != 0) {
+        crank_revolution_diff = new_cumulative_crank_revs - cscs_instantanious_data.oldCrankRevolution.value;
+    }
+    if (crank_event_time_diff > 0) {
+        crank_event_time_diff = crank_event_time_diff / 1024.0;
+        travel_cadence = ((crank_revolution_diff / crank_event_time_diff) * 60); //rp..
+    }
+
+    cscs_instantanious_data.oldCrankRevolution.value = new_cumulative_crank_revs;
+		cscs_instantanious_data.oldCrankRevolution.is_read = false;
+    cscs_instantanious_data.oldCrankEventTime.value = new_crank_event_time;
+		cscs_instantanious_data.oldCrankEventTime.is_read= false;
+		cscs_instantanious_data.travel_cadence.value = travel_cadence;
+		cscs_instantanious_data.travel_cadence.is_read = false;
+		
+		return crank_revolution_diff;
+}
+
+static void cscsApp_decode_cscs_meas_data ( ble_cscs_c_meas_t *csc_meas_data)
+{
+    double            wheel_rev_diff              = 0.0;
+		double            crank_rev_diff              = 0.0;
+    double            wheel_to_crank_diff_ratio   = 0.0;
+	
+	  if (csc_meas_data == NULL){
+				NRF_LOG_ERROR("cscsApp_decode_cscs_meas_data called with NULL argument\r\n");
+			  return;
+		}
+    
+    if (csc_meas_data->is_wheel_rev_data_present)
+    {
+        wheel_rev_diff = process_wheel_data(csc_meas_data->cumulative_wheel_revs, csc_meas_data->last_wheel_event_time);
+        if (csc_meas_data->is_crank_rev_data_present)
+        {
+            crank_rev_diff = process_crank_data(csc_meas_data->cumulative_crank_revs, csc_meas_data->last_crank_event_time);
+            if (crank_rev_diff > 0) {
+                wheel_to_crank_diff_ratio = wheel_rev_diff/crank_rev_diff;
+                NRF_LOG_INFO("Wheel to crank diff ratio = %f \r\n",wheel_to_crank_diff_ratio);
+            }
+        }
+    }
+    else
+    {
+        if (csc_meas_data->is_crank_rev_data_present)
+        {
+						crank_rev_diff = process_crank_data(csc_meas_data->cumulative_crank_revs, csc_meas_data->last_crank_event_time);
+					  NRF_LOG_INFO("Crank revolution diff = %l \r\n",crank_rev_diff);
+        }
+    }
+}
+
+static void cscsApp_debug_print_inst_data(){
+		
+		NRF_LOG_INFO("\r\n");
+		NRF_LOG_INFO("oldWheelRevolution  = %d \r\n", cscs_instantanious_data.oldWheelRevolution.value);
+		NRF_LOG_INFO("oldCrankRevolution  = %d \r\n", cscs_instantanious_data.oldCrankRevolution.value);
+		NRF_LOG_INFO("travelDistance      = %d \r\n", cscs_instantanious_data.travelDistance.value);
+		NRF_LOG_INFO("totalTravelDistance = %d \r\n", cscs_instantanious_data.totalTravelDistance.value);
+		NRF_LOG_INFO("speed               = %d \r\n", cscs_instantanious_data.speed.value);
+		NRF_LOG_INFO("travel_cadence      = %d \r\n", cscs_instantanious_data.travel_cadence.value);
+		NRF_LOG_INFO("oldWheelEventTime   = %d \r\n", cscs_instantanious_data.oldWheelEventTime.value);
+		NRF_LOG_INFO("oldCrankEventTime   = %d \r\n", cscs_instantanious_data.oldCrankEventTime.value);
+		
+}
+
+
 /**@brief Cycling Speed and Cadence Collector Handler.
  */
 static void cscsApp_cscs_c_evt_handler(ble_cscs_c_t * p_csc_c, ble_cscs_c_evt_t * p_csc_c_evt)
@@ -67,7 +210,10 @@ static void cscsApp_cscs_c_evt_handler(ble_cscs_c_t * p_csc_c, ble_cscs_c_evt_t 
 
         case BLE_CSCS_C_EVT_CSC_NOTIFICATION:
         {
-            NRF_LOG_INFO("\r\n");
+            cscsApp_decode_cscs_meas_data(&(p_csc_c_evt->params.csc_meas));
+					  cscsApp_debug_print_inst_data();
+					  /*
+					  NRF_LOG_INFO("\r\n");
             if(p_csc_c_evt->params.csc_meas.is_wheel_rev_data_present){
 								NRF_LOG_INFO("Comulative Wheel Revolution   = %d\r\n",
 							                p_csc_c_evt->params.csc_meas.cumulative_wheel_revs);
@@ -81,6 +227,7 @@ static void cscsApp_cscs_c_evt_handler(ble_cscs_c_t * p_csc_c, ble_cscs_c_evt_t 
 							NRF_LOG_INFO("Last Crank Event time         = %d\r\n",
 							                p_csc_c_evt->params.csc_meas.last_crank_event_time);
 						}
+					*/
 						
 						break;
         }
@@ -89,6 +236,7 @@ static void cscsApp_cscs_c_evt_handler(ble_cscs_c_t * p_csc_c, ble_cscs_c_evt_t 
             break;
     }
 }
+
 
 /**********************************************************************************************
 * PUBLIC FUCNCTIONS
