c5793ad7f3916c940a3bb1391cce09430c2be44a
Ali Ba Wazir <alwaziry20@gmail.com>
2016-10-23 06:14:38 -0400

    Now, all SPI setter commands are handled by SPIS sim driver. Reassigned the hex codes of getter commands according to updates in SPI interaction spec

---
+++ NRF51822/NRF_SDK12/examples/ble_central/experimental/ble_app_cscs_c/pca10028/s130/arm5_no_packs/gear_selection_algorithm.c
@@ -0,0 +1,52 @@
+/*
+* Copyright (c) 2016 Shifty: Automatic Gear Selection System. All Rights Reserved.
+ *
+ * The code developed in this file is for the 4th year project: Shifty. The use,
+ * copying, transfer or disclosure of such information is prohibited except by express written
+ * agreement with the Shifty team.
+ * Aportion of the source code here has been adopted from the Nordic Semiconductor' RSCS main application
+ * Created by: Ali Ba Wazir
+ *
+ */
+
+/**
+ * @brief Gear Selection Algoirthm.
+ *
+ */
+ 
+ /**********************************************************************************************
+* INCLUDES
+***********************************************************************************************/
+#include "sdk_config.h"
+#include "boards.h"
+#include "app_error.h"
+#include <string.h>
+#define NRF_LOG_MODULE_NAME "ALGORITHM APP"
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+
+#include "../spis_app.h"
+
+/**********************************************************************************************
+* MACRO DEFINITIONS
+***********************************************************************************************/
+
+
+/**********************************************************************************************
+* TYPE DEFINITIONS
+***********************************************************************************************/
+
+
+/**********************************************************************************************
+* STATIC VARIABLES
+***********************************************************************************************/
+
+
+/**********************************************************************************************
+* STATIC FUCNCTIONS
+***********************************************************************************************/
+
+
+/**********************************************************************************************
+* PUBLIC FUCNCTIONS
+***********************************************************************************************/
