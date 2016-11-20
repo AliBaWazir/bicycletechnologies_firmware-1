54c6cae1d55d3bfe0dd3f00be2a590440f720440
Ali Ba Wazir <alwaziry20@gmail.com>
2016-11-20 17:52:43 -0500

    Added template files for i2c_sim_driver.c and i2c_sim_driver.h

--- NRF51822/NRF_SDK12/examples/ble_central/experimental/ble_app_cscs_c/pca10028/s130/arm5_no_packs/i2c_sim_driver.c
+++ NRF51822/NRF_SDK12/examples/ble_central/experimental/ble_app_cscs_c/pca10028/s130/arm5_no_packs/i2c_sim_driver.c
@@ -5,12 +5,11 @@
  * copying, transfer or disclosure of such information is prohibited except by express written
  * agreement with the Shifty team.
  * Created by: Ali Ba Wazir
- * Code is based on example code of nRF SDK v.12 under /examples/peripheral/twi_sensor
  * Nov 2016
  */
 
 /**
- * @brief BLE Connection Manager application file.
+ * @brief I2C simulation driver file.
  *
  */
  
@@ -25,22 +24,16 @@
 #include "nrf_delay.h"
 #include "app_error.h"
 #include "app_timer.h"
-#define NRF_LOG_MODULE_NAME "I2C APP"
+#define NRF_LOG_MODULE_NAME "I2C SIM_DRIVER"
 #include "nrf_log.h"
 #include "nrf_log_ctrl.h"
 
-#include "i2c_app.h"
 #include "i2c_sim_driver.h"
 
 
 /**********************************************************************************************
 * MACRO DEFINITIONS
 ***********************************************************************************************/
-#define I2C_IN_SIMULATION_MODE   1        /*This is set to 1 only if the I2C app is running in debugging mode 
-                                           *to charecterize the I2C link between nRF module and gear controller
-										   */
-
-#define I2C_BASE_REGISTER_ADDRESS   0x1234         //address of I2C base register
 
 
 /**********************************************************************************************
@@ -64,14 +57,11 @@
 /**
  * @brief Connection Manager App initialization.
  */
-bool i2cApp_init(void){
+bool i2cSimDriver_init(void){
 	
 	bool         ret_code         = true;
 	ret_code_t   nrf_err          = NRF_SUCCESS;
 	
-	if (I2C_IN_SIMULATION_MODE){
-		ret_code = i2cSimDriver_init();
-	}
 		
 	return ret_code;	
 }
---
+++ NRF51822/NRF_SDK12/examples/ble_central/experimental/ble_app_cscs_c/pca10028/s130/arm5_no_packs/i2c_sim_driver.h
@@ -0,0 +1,19 @@
+#ifndef I2C_SIM_DRIVER_H__
+#define I2C_SIM_DRIVER_H__
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+bool i2cSimDriver_init(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // I2C_SIM_DRIVER_H__
+
