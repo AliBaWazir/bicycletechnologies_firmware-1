84e5495a98a5a40343be96a9f254eab91d6efd41
Ali Ba Wazir <alwaziry20@gmail.com>
2016-11-27 23:42:24 -0500

    Added defines for SPI commands and data availability flags

--- NRF51822/NRF_SDK12/examples/ble_central/experimental/ble_app_cscs_c/pca10028/s130/arm5_no_packs/spis_app.c
+++ NRF51822/NRF_SDK12/examples/ble_central/experimental/ble_app_cscs_c/pca10028/s130/arm5_no_packs/spis_app.c
@@ -30,25 +30,59 @@
 /**********************************************************************************************
 * MACRO DEFINITIONS
 ***********************************************************************************************/
-#define SPIS_INSTANCE 1 /**< SPIS instance index. */
+#define SPI_INSTANCE 1 /**< SPIS instance index. */
 
-//SPI request definitions
-#define SPIS_REQUEST_AVAILABLE_DATA     0xDA
-#define SPIS_REQUEST_SPEED              0x01
-#define SPIS_REQUEST_CADENCE            0x02
-#define SPIS_REQUEST_DISTANCE           0x03
-#define SPIS_REQUEST_HR                 0x04
-#define SPIS_REQUEST_BATTERY            0x05
+/*************************************************
+ *SPI GETTER command definitions
+ *************************************************/
+//Check Data Availabilable Flags 
+#define SPI_GET_AVAILABLE_DATA_FLAGS     0xDA
 
-#define SPIS_SET_CADENCE_SETPOINT       0x10
-#define SPIS_SET_WHEEL_DIAMETER         0x20
-#define SPIS_SET_GEAR_COUNT             0x30
-#define SPIS_SET_TEETH_COUNT            0x40
+//Group 0: Shifting Algorithm Parameters
+#define SPI_GET_SPEED                  0x01
+#define SPI_GET_CADENCE                0x02
+#define SPI_GET_DISTANCE               0x03
+#define SPI_GET_HR                     0x04
+#define SPI_GET_CADENCE_SETPOINT       0x05
+
+//Group 1: Bike configuration
+#define SPI_GET_WHEEL_DIAMETER         0x10
+#define SPI_GET_GEAR_COUNT             0x11
+#define SPI_GET_TEETH_COUNT_ON_GEAR    0x12
+
+//Group 2: Bluetooth Device Configuration								
+#define SPI_GET_ADV_DEVICE_COUNT            0x20
+#define SPI_GET_ADV_DEVICE_DATA_BY_INDEX    0x21
+#define SPI_GET_PAIRED_DEVICES              0x22
+#define SPI_GET_CONNECTED_DEVICES           0x23
+#define SPI_GET_CSC_DEVICE_NAME             0x27
+#define SPI_GET_HR_DEVICE_NAME              0x28
+#define SPI_GET_PHONE_DEVICE_NAME           0x29
 
 
-//SPI argument indeces
+/*************************************************
+ *SPI SETTER command definitions
+ *************************************************/
+//Group 0: Shifting Algorithm Parameters							
+#define SPI_SET_CADENCE_SETPOINT       0x08
+#define SPI_SET_GEAR_LEVEL_LOCKED      0x09
+
+//Group 1: Bike configuration
+#define SPI_SET_WHEEL_DIAMETER         0x18
+#define SPI_SET_GEAR_COUNT             0x19
+#define SPI_SET_TEETH_COUNT_ON_GEAR    0x1A
+
+//Group 2: Bluetooth Device Configuration
+#define SPI_BEGIN_SCAN                 0x2A
+#define SPI_CONNECT_DEVICE             0x2B
+#define SPI_FORGET_DEVICE              0x2C
+
+
+/*************************************************
+ *SPI argument indeces
+ *************************************************/
 #define INDEX_ARG_BASE             1                  // second bit rx[1] denotes the first argument
-#define INDEX_ARG_SETPOINT         INDEX_ARG_BASE     // index of argument setpoint used with SPIS_SET_CADENCE_SETPOINT
+#define INDEX_ARG_SETPOINT         INDEX_ARG_BASE     // index of argument setpoint used with SPI_SET_CADENCE_SETPOINT
 #define INDEX_ARG_WHEEL_DIAMETER   INDEX_ARG_BASE
 #define INDEX_ARG_GEAR_TYPE        INDEX_ARG_BASE
 #define INDEX_ARG_GEAR_COUNT       (INDEX_ARG_BASE+1) //if value of this arg is 1, it is cranck gear, 2 for wheel
@@ -56,10 +90,28 @@
 #define INDEX_ARG_TEETH_COUNT      (INDEX_ARG_BASE+2) //teeth count in a gear defined by INDEX_ARG_GEAR_TYPE and INDEX_ARG_GEAR_INDEX
 
 
-#define SPIS_DRIVER_SIM_MODE 1 /*
-								This flag is set to 1 only if SPI slave interaction
-								is in simulation mode.
-								*/
+/*************************************************
+ *SPI data availability FLAGS
+ *************************************************/
+#define SPI_AVAIL_FLAG_SPEED                  (0x01<<0)
+#define SPI_AVAIL_FLAG_CADENCE                (0x01<<1)
+#define SPI_AVAIL_FLAG_DISTANCE               (0x01<<2)
+#define SPI_AVAIL_FLAG_HR                     (0x01<<3)
+#define SPI_AVAIL_FLAG_BIKE_CONFIG_PAMAS      (0x01<<8)
+#define SPI_AVAIL_FLAG_ADVER_DEVICES_COUNT    (0x01<<16)
+#define SPI_AVAIL_FLAG_PAIRED_DEVICES         (0x01<<17)
+#define SPI_AVAIL_FLAG_CONNECTED_DEVICES      (0x01<<18)
+#define SPI_AVAIL_FLAG_CSC_DEVICE_NAME        (0x01<<19) 
+#define SPI_AVAIL_FLAG_HR_DEVICE_NAME         (0x01<<20)
+#define SPI_AVAIL_FLAG_PHONE_DEVICE_NAME      (0x01<<21)
+
+
+
+
+#define SPI_DRIVER_SIM_MODE 1 /****************************************************
+							   *This flag is set to 1 only if SPI slave interaction
+							   *is in simulation mode.
+							   ***************************************************/
 
 /**********************************************************************************************
 * TYPE DEFINITIONS
@@ -69,7 +121,7 @@
 /**********************************************************************************************
 * STATIC VARIABLES
 ***********************************************************************************************/
-static const nrf_drv_spis_t spis = NRF_DRV_SPIS_INSTANCE(SPIS_INSTANCE);/**< SPIS instance. */
+static const nrf_drv_spis_t spis = NRF_DRV_SPIS_INSTANCE(SPI_INSTANCE);/**< SPIS instance. */
 
 
 static uint8_t       m_tx_buf[4];          /**< TX buffer. */
@@ -104,12 +156,12 @@ static void spisApp_event_handler(nrf_drv_spis_event_t event)
 		switch (command){
 			
 			/**************************** GETTERS ********************************/
-			case SPIS_REQUEST_AVAILABLE_DATA:
+			case SPI_GET_AVAILABLE_DATA_FLAGS:
 				memcpy(m_tx_buf, bitField, 4); // Copy current available data into tx buffer in preperation for clock out. 
 			break;
 			
-			case SPIS_REQUEST_SPEED:
-				if (SPIS_DRIVER_SIM_MODE){
+			case SPI_GET_SPEED:
+				if (SPI_DRIVER_SIM_MODE){
 					m_tx_buf[0] = spisSimDriver_get_current_data(CSCS_DATA_SPEED);
 				} else {
 					/*TODO: get data from CSCS app*/
@@ -117,8 +169,8 @@ static void spisApp_event_handler(nrf_drv_spis_event_t event)
 				}
 			break;
 			
-			case SPIS_REQUEST_CADENCE:
-				if (SPIS_DRIVER_SIM_MODE){
+			case SPI_GET_CADENCE:
+				if (SPI_DRIVER_SIM_MODE){
 					m_tx_buf[0] = spisSimDriver_get_current_data(CSCS_DATA_CADENCE);
 				} else {
 					/*TODO: get data from CSCS app*/
@@ -126,8 +178,8 @@ static void spisApp_event_handler(nrf_drv_spis_event_t event)
 				}
 			break;
 				
-			case SPIS_REQUEST_DISTANCE:
-				if (SPIS_DRIVER_SIM_MODE){
+			case SPI_GET_DISTANCE:
+				if (SPI_DRIVER_SIM_MODE){
 					m_tx_buf[0] = spisSimDriver_get_current_data(CSCS_DATA_DISTANCE);
 				} else {
 					/*TODO: get data from CSCS app*/
@@ -135,47 +187,47 @@ static void spisApp_event_handler(nrf_drv_spis_event_t event)
 				}
 			break;
 			
-			case SPIS_REQUEST_HR:
-				if (SPIS_DRIVER_SIM_MODE){
+			case SPI_GET_HR:
+				if (SPI_DRIVER_SIM_MODE){
 					m_tx_buf[0] = spisSimDriver_get_current_data(CSCS_DATA_HR);
 				} else {
 					/*TODO: get data from CSCS app*/
 					m_tx_buf[0] = 0xEA;
 				}
 			break;
-			
-			case SPIS_REQUEST_BATTERY:
+			//not used
+			//case SPI_GET_BATTERY:
 				/*TODO: figure out which device's battery*/
-				m_tx_buf[0] = 0xBA;
-			break;
+				//m_tx_buf[0] = 0xBA;
+			//break;
 			
 			/**************************** SETTERS ********************************/
-			case SPIS_SET_CADENCE_SETPOINT:
-				if (SPIS_DRIVER_SIM_MODE){
+			case SPI_SET_CADENCE_SETPOINT:
+				if (SPI_DRIVER_SIM_MODE){
 					spisSimDriver_set_cadence_setpoint(m_tx_buf[INDEX_ARG_SETPOINT]);
 				} else {
 					algorithmApp_set_cadence_setpoint (m_tx_buf[INDEX_ARG_SETPOINT]);
 				}
 			break;
 				
-			case SPIS_SET_WHEEL_DIAMETER:
-				if (SPIS_DRIVER_SIM_MODE){
+			case SPI_SET_WHEEL_DIAMETER:
+				if (SPI_DRIVER_SIM_MODE){
 					spisSimDriver_set_wheel_diameter(m_tx_buf[INDEX_ARG_WHEEL_DIAMETER]);
 				} else {
 					algorithmApp_set_wheel_diameter(m_tx_buf[INDEX_ARG_WHEEL_DIAMETER]);
 				}
 			break;
 				
-			case SPIS_SET_GEAR_COUNT:
-				if (SPIS_DRIVER_SIM_MODE){
+			case SPI_SET_GEAR_COUNT:
+				if (SPI_DRIVER_SIM_MODE){
 					spisSimDriver_set_gear_count(m_tx_buf[INDEX_ARG_GEAR_TYPE], m_tx_buf[INDEX_ARG_GEAR_COUNT]);
 				} else {
 					algorithmApp_set_gear_count(m_tx_buf[INDEX_ARG_GEAR_TYPE], m_tx_buf[INDEX_ARG_GEAR_COUNT]);
 				}
 			break;
 				
-			case SPIS_SET_TEETH_COUNT:
-				if (SPIS_DRIVER_SIM_MODE){
+			case SPI_SET_TEETH_COUNT_ON_GEAR:
+				if (SPI_DRIVER_SIM_MODE){
 					spisSimDriver_set_teeth_count(m_tx_buf[INDEX_ARG_GEAR_TYPE], m_tx_buf[INDEX_ARG_GEAR_INDEX], m_tx_buf[INDEX_ARG_TEETH_COUNT]);
 				} else {
 					algorithmApp_set_teeth_count(m_tx_buf[INDEX_ARG_GEAR_TYPE], m_tx_buf[INDEX_ARG_GEAR_INDEX], m_tx_buf[INDEX_ARG_TEETH_COUNT]);
@@ -225,7 +277,7 @@ bool spisApp_init(void)
  	spisApp_config();
 	
 	// if data need to be generated in simulation mode, initialize SPIS simulation driver
-	if (ret_code && SPIS_DRIVER_SIM_MODE){
+	if (ret_code && SPI_DRIVER_SIM_MODE){
 		ret_code = spisSimDriver_init();
 	}
 
