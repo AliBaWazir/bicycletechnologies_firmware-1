b11dcd7e6ac7e0b49cd6e2ecb1091c78e618576c
Ali Ba Wazir <alwaziry20@gmail.com>
2017-01-02 22:49:42 -0500

    Added the necessary code to application_fds_app to allow writing and reading data to/from flash data storage. The default cadence point=80 RPM is written during ini time if not existed in flash storage

--- NRF51822/NRF_SDK12/examples/ble_central/experimental/ble_app_cscs_c/pca10028/s130/arm5_no_packs/algorithm_app.c
+++ NRF51822/NRF_SDK12/examples/ble_central/experimental/ble_app_cscs_c/pca10028/s130/arm5_no_packs/algorithm_app.c
@@ -25,6 +25,8 @@
 #define NRF_LOG_MODULE_NAME "ALGORITHM APP"
 #include "nrf_log.h"
 #include "nrf_log_ctrl.h"
+#include "drivers_commons.h"
+#include "application_fds_app.h"
 
 #include "algorithm_app.h"
 
@@ -32,30 +34,32 @@
 /**********************************************************************************************
 * MACRO DEFINITIONS
 ***********************************************************************************************/
-
+#define DEFAULT_CADENCE_SETPOINT_RPM   80
 
 /**********************************************************************************************
 * TYPE DEFINITIONS
 ***********************************************************************************************/
 typedef enum{
-	USER_DEFINED_PROPERTIES_UPDATE
+	BIKE_CONFIG_DATA_UPDATE,
+	CADENCE_SETPOINT_UPDATE
 } algorithmApp_event_e;
 
 /**********************************************************************************************
 * STATIC VARIABLES
 ***********************************************************************************************/
-static user_defined_properties_t  user_defined_properties;  //will contain user defined data
+static user_defined_bike_config_data_t  user_defined_bike_config_data;  //will contain user defined bike configuration data
 
 static bool  gear_level_locked = false;                     /*TODO: this boolean has to be checked before running the gear shifting algorithm
 															 *If set to true, lock gear level at the middle to maximize efficiency.  
 															 */
+static uint32_t  cadence_setpoint_rpm= 0;
 
 /**********************************************************************************************
 * STATIC FUCNCTIONS
 ***********************************************************************************************/
 static void algorithmApp_fire_event (algorithmApp_event_e event){
 	switch (event){
-		case USER_DEFINED_PROPERTIES_UPDATE:
+		case BIKE_CONFIG_DATA_UPDATE:
 			//update user defined properties stored in ROM
 			/*TODO: implement encoding function to encode user_defined_properties serially*/
 		break;
@@ -73,19 +77,19 @@ static void algorithmApp_fire_event (algorithmApp_event_e event){
 
 /************************* GETTERS ****************************/
 uint8_t algorithmApp_get_cadence_setpoint(void){
-	return (uint8_t)user_defined_properties.cadence_setpoint_rpm;
+	return (uint8_t)cadence_setpoint_rpm;
 }
 
 uint8_t algorithmApp_get_wheel_diameter_cm(void){
-	return (uint8_t)user_defined_properties.wheel_diameter_cm;
+	return (uint8_t)user_defined_bike_config_data.wheel_diameter_cm;
 }
 
 uint8_t algorithmApp_get_gears_count_crank(void){
-	return (uint8_t)user_defined_properties.crank_gears_count;
+	return (uint8_t)user_defined_bike_config_data.crank_gears_count;
 }
 
 uint8_t algorithmApp_get_gears_count_wheel(void){
-	return (uint8_t)user_defined_properties.wheel_gears_count;
+	return (uint8_t)user_defined_bike_config_data.wheel_gears_count;
 }
 
 uint8_t algorithmApp_get_teeth_count(uint8_t gear_type, uint8_t gear_index){
@@ -95,9 +99,9 @@ uint8_t algorithmApp_get_teeth_count(uint8_t gear_type, uint8_t gear_index){
 		return 0;
 	} else{
 		if(gear_type == CRANK_IDENTIFIER){
-			return (uint8_t)user_defined_properties.crank_gears_teeth_count[gear_index];
+			return (uint8_t)user_defined_bike_config_data.crank_gears_teeth_count[gear_index];
 		} else if (gear_type == WHEEL_IDENTIFIER){
-			return (uint8_t)user_defined_properties.wheel_gears_teeth_count[gear_index];
+			return (uint8_t)user_defined_bike_config_data.wheel_gears_teeth_count[gear_index];
 		}else{
 			NRF_LOG_ERROR("algorithmApp_get_teeth_count: called with unknown gear_type=0x%x\r\n", gear_type);
 			return 0;
@@ -107,10 +111,10 @@ uint8_t algorithmApp_get_teeth_count(uint8_t gear_type, uint8_t gear_index){
 
 /************************* SETTERS ****************************/
 void algorithmApp_set_cadence_setpoint(uint8_t new_cadence_setpoint){
-	user_defined_properties.cadence_setpoint_rpm = new_cadence_setpoint;
+	cadence_setpoint_rpm = new_cadence_setpoint;
 	
 	//fire update event to encode updated data in ROM
-	algorithmApp_fire_event(USER_DEFINED_PROPERTIES_UPDATE);
+	algorithmApp_fire_event(CADENCE_SETPOINT_UPDATE);
 	
 	NRF_LOG_INFO("algorithmApp_set_cadence_setpoint: cadence_setpoint is changed to= %d .\r\n",new_cadence_setpoint);
 }
@@ -122,10 +126,10 @@ void algorithmApp_set_gear_level_locked(){
 }
 
 void algorithmApp_set_wheel_diameter (uint8_t new_wheel_diameter){
-	user_defined_properties.wheel_diameter_cm = new_wheel_diameter;
+	user_defined_bike_config_data.wheel_diameter_cm = new_wheel_diameter;
 	
 	//fire update event to encode updated data in ROM
-	algorithmApp_fire_event(USER_DEFINED_PROPERTIES_UPDATE);
+	algorithmApp_fire_event(BIKE_CONFIG_DATA_UPDATE);
 	NRF_LOG_INFO("algorithmApp_set_wheel_diameter: wheel_diameter is changed to= %d .\r\n",new_wheel_diameter);
 }
 
@@ -135,12 +139,12 @@ bool algorithmApp_set_gear_count(uint8_t crank_gears_count, uint8_t wheel_gears_
 		NRF_LOG_WARNING("algorithmApp_set_gear_count: crank_gears_count= %d or wheel_gears_count= %d is greater than MAX_GEARS_COUNT.\r\n",crank_gears_count, wheel_gears_count);
 		return false;
 	} else{
-		user_defined_properties.crank_gears_count= crank_gears_count;
-		user_defined_properties.wheel_gears_count= wheel_gears_count;
+		user_defined_bike_config_data.crank_gears_count= crank_gears_count;
+		user_defined_bike_config_data.wheel_gears_count= wheel_gears_count;
 	}
 	
 	//fire update event to encode updated data in ROM
-	algorithmApp_fire_event(USER_DEFINED_PROPERTIES_UPDATE);
+	algorithmApp_fire_event(BIKE_CONFIG_DATA_UPDATE);
 	NRF_LOG_INFO("gear_count is changed to crank_gears_count= %d, wheel_gears_count= %d\r\n",crank_gears_count, wheel_gears_count);
 	return true;
 }
@@ -148,15 +152,15 @@ bool algorithmApp_set_gear_count(uint8_t crank_gears_count, uint8_t wheel_gears_
 void algorithmApp_set_teeth_count (uint8_t gear_type, uint8_t gear_index, uint8_t new_teeth_count){
 	
 	if (gear_type == CRANK_IDENTIFIER){
-		user_defined_properties.crank_gears_teeth_count[gear_index] = new_teeth_count;
+		user_defined_bike_config_data.crank_gears_teeth_count[gear_index] = new_teeth_count;
 	} else if (gear_type == WHEEL_IDENTIFIER){
-		user_defined_properties.wheel_gears_teeth_count[gear_index] = new_teeth_count;
+		user_defined_bike_config_data.wheel_gears_teeth_count[gear_index] = new_teeth_count;
 	} else {
 		NRF_LOG_ERROR("algorithmApp_set_teeth_count: called with invalid gear_type= %d\r\n",gear_type);
 	}
 	
     //fire update event to encode updated data in ROM
-	algorithmApp_fire_event(USER_DEFINED_PROPERTIES_UPDATE);
+	algorithmApp_fire_event(BIKE_CONFIG_DATA_UPDATE);
     NRF_LOG_INFO("algorithmApp_set_teeth_count: teeth_count for gear type=%d in gear index[%d] is updated to count= %d.\r\n", gear_type, gear_index, new_teeth_count);
 	
 }
@@ -165,9 +169,37 @@ bool algorithmApp_init(void){
 	
 	bool ret_code  = true;
 	
-	memset (&user_defined_properties, 0, sizeof(user_defined_properties_t));
+	memset (&user_defined_bike_config_data, 0, sizeof(user_defined_bike_config_data_t));
 	
-	/*TODO: decode bicycle preoperties defined by user to a static struct*/
+	/*decode bicycle preoperties defined by user from FDS to a static struct*/
+	if(!applicationFdsApp_init()){
+		NRF_LOG_ERROR("algorithmApp_init: applicationFdsApp_init() failed\r\n");
+		ret_code= false;
+	} else{
+		//read cadence setpoint from FDS
+		if(!applicationFdsApp_fds_read(USER_DEFINED_CADENCE_SETPOINT, sizeof(cadence_setpoint_rpm), (uint8_t*) &cadence_setpoint_rpm)){
+			
+			NRF_LOG_WARNING("algorithmApp_init: applicationFdsApp_fds_read() failed. Default value will be written\r\n");
+			/*TODO: figure out a way to ask user to enter cadence setpoint for fisrt time use.*/
+			
+			//if cadence setpoint is not already set, set it to default value
+			uint32_t cadence_setpoint_value = DEFAULT_CADENCE_SETPOINT_RPM;
+			if(!applicationFdsApp_fds_store(USER_DEFINED_CADENCE_SETPOINT, sizeof(cadence_setpoint_rpm), (uint8_t*)&cadence_setpoint_value)){
+				NRF_LOG_ERROR("algorithmApp_init: applicationFdsApp_fds_write() failed\r\n");
+				ret_code= false;
+			} else{
+				//read back the written value
+				if(!applicationFdsApp_fds_read(USER_DEFINED_CADENCE_SETPOINT, sizeof(cadence_setpoint_rpm), (uint8_t*) &cadence_setpoint_rpm)){
+					NRF_LOG_ERROR("algorithmApp_init: applicationFdsApp_fds_read() failed\r\n");
+					ret_code= false;
+				}
+			}
+			
+		} else{
+			NRF_LOG_INFO("algorithmApp_init: cadence setpoint=%d is retrieved from flash sata storage\r\n", cadence_setpoint_rpm);
+		}
+	}
+	
 	
 	return ret_code;
 }
